3805df2613c8c0b2c08968ae0228d94b939670a0
James Kong <jkong@musiclifeltd.com>
2015-01-04 17:16:02 +0800

    adde video Mananger

---
+++ src/VideoGrabberManager.cpp
@@ -0,0 +1,9 @@
+//
+//  VideoGrabberManager.cpp
+//  wlbankinteractive
+//
+//  Created by Kong king sin on 2/1/15.
+//
+//
+
+#include "VideoGrabberManager.h"
---
+++ src/VideoGrabberManager.h
@@ -0,0 +1,14 @@
+//
+//  VideoGrabberManager.h
+//  wlbankinteractive
+//
+//  Created by Kong king sin on 2/1/15.
+//
+//
+
+#ifndef __wlbankinteractive__VideoGrabberManager__
+#define __wlbankinteractive__VideoGrabberManager__
+
+#include <stdio.h>
+
+#endif /* defined(__wlbankinteractive__VideoGrabberManager__) */
